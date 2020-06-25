946e434faa537e59c0112a5ba13f5ffbe9c57119
Larisa Breskaja <lora.breskaja@gmail.com>
2020-06-26 01:35:59 +0300

    Update plates.service.ts

--- src/app/plates/plates.service.ts
+++ src/app/plates/plates.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Plate } from './plate.model';
-import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Plate } from './plate.model';
 
 @Injectable({providedIn: 'root'})
 export class PlatesService {
@@ -11,21 +13,66 @@ export class PlatesService {
   constructor(private http: HttpClient) {}
 
   getPlates() {
-    this.http.get<{message: string, plates: Plate[]}>('http://localhost:3000/api/posts')
-      .subscribe((plateData) => {
-        this.plates = plateData.plates;
-        this.platesUpdated.next([...this.plates]);
-      });
+    this.http
+      .get<{message: string, plates: any }>(
+        "http://localhost:3000/api/plates"
+      )
+      .pipe(map((plateData) => {
+        return plateData.plates.map(plate => {
+          return {
+            name: plate.name,
+            plateNumber: plate.plateNumber,
+            id: plate._id
+          };
+         });
+      }))
+      .subscribe(transformedPlates => {
+          this.plates = transformedPlates;
+          this.platesUpdated.next([...this.plates]);
+        });
   }
 
   getPlateUpdateListener() {
     return this.platesUpdated.asObservable();
   }
 
-  addPlate(name: string, plateNumber: string) {
-    const plate: Plate = {id: null, name: name, plateNumber: plateNumber};
-    this.plates.push(plate);
-    this.platesUpdated.next([...this.plates]);
+  getPlate(id: string) {
+    return this.http.get<{_id: string, name: string, plateNumber: string}>(
+      'http://localhost:3000/api/plates/' + id
+      );
   }
 
+  addPlate(name: string, plateNumber: string) {
+    const plate: Plate = {id: null, name: name, plateNumber: plateNumber};
+    this.http
+      .post<{message: string, plateId: string }>('http://localhost:3000/api/plates', plate)
+      .subscribe((responseData) => {
+        const id = responseData.plateId;
+        plate.id = id;
+        this.plates.push(plate);
+        this.platesUpdated.next([...this.plates]);
+    });
+  }
+
+  updatePlate(id: string, name: string, plateNumber: string) {
+    const plate: Plate = { id: id, name: name, plateNumber: plateNumber };
+    this.http
+      .put('http://localhost:3000/api/plates/' + id, plate)
+      .subscribe(response => {
+        const updatesPlates = [...this.plates];
+        const oldPlateIndex = updatesPlates.findIndex(p => p.id === plate.id);
+        updatesPlates[oldPlateIndex] = plate;
+        this.plates = updatesPlates;
+        this.platesUpdated.next([...this.plates]);
+      });
+  }
+
+  deletePlate(plateId: string) {
+    this.http.delete('http://localhost:3000/api/plates/' + plateId)
+      .subscribe(() => {
+        const updatedPlates = this.plates.filter(plate => plate.id !== plateId);
+        this.plates = updatedPlates;
+        this.platesUpdated.next([...this.plates]);
+      });
+  }
 }
