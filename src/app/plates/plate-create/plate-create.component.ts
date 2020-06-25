ec81b5bdb12bef288878c1632bc6cc1ffa972e27
Larisa Breskaja <lora.breskaja@gmail.com>
2020-06-26 01:35:53 +0300

    Update plate-create.component.ts

--- src/app/plates/plate-create/plate-create.component.ts
+++ src/app/plates/plate-create/plate-create.component.ts
@@ -1,23 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
 import { PlatesService } from '../plates.service';
+import { Plate } from '../plate.model';
 
 @Component({
   selector: 'app-plate-create',
   templateUrl: './plate-create.component.html',
   styleUrls: ['./plate-create.component.css']
 })
-export class PlateCreateComponent {
+export class PlateCreateComponent implements OnInit {
   enteredName = '';
   enteredPlate = '';
+  plate: Plate;
+  private mode = 'create';
+  private plateId: string;
 
-  constructor(public platesService: PlatesService) {}
+  constructor(public platesService: PlatesService, public route: ActivatedRoute) {}
 
-  onAddPlate(form: NgForm) {
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('plateId')) {
+        this.mode = 'edit';
+        this.plateId = paramMap.get('plateId');
+        this.platesService.getPlate(this.plateId).subscribe(plateData => {
+          this.plate = {id: plateData._id, name: plateData.name, plateNumber: plateData.plateNumber};
+        });
+      } else {
+        this.mode = 'create';
+        this.plateId = null;
+      }
+    });
+  }
+
+  onSavePlate(form: NgForm) {
     if (form.invalid) {
       return;
     }
-   this.platesService.addPlate(form.value.name, form.value.plateNumber);
-   form.resetForm();
-   }
+    if (this.mode === 'create') {
+      this.platesService.addPlate(form.value.name, form.value.plateNumber);
+    } else {
+      this.platesService.updatePlate(
+        this.plateId,
+        form.value.name,
+        form.value.plateNumber
+        );
+      }
+      form.resetForm();
+    }
 }
